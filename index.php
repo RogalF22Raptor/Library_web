c2b1c69ebad04b42866a983016f6bbd2b108a06c
Patryk Rogalski <patrykrogalski@vip.onet.pl>
2023-06-07 10:20:32 +0200

    test commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8"/>
+	<title>Library</title>
+	<meta name="description" content="You library"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link  rel="stylesheet" href="css/main-page.css" type="text/css"/>
+</head>
